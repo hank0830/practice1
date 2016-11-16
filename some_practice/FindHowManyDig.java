d0b6fd1bc12e6a95a1c339dea7c87039b84f8a48
hank0830 <abcdefg123855@gmail.com>
2016-11-16 22:57:15 +0800

    10/16提交

---
+++ some_practice/FindHowManyDig.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class FindHowManyDig{
+	public static  void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		String s = input.nextLine();
+		char[] array = s.toCharArray();
+		for(int i = 0 ;i<= s.length;i++){
+			if(char[i])
+		}
+	}
+	
+}
--- some_practice/prime_year.java
+++ some_practice/prime_year.java
@@ -1,6 +1,33 @@
 import java.util.Scanner;
+import java.util.*;
 class prime_year{
 	public static void main (String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("輸入你要的年份間距");
+		int a = input.nextInt();
+		int b = input.nextInt();
+		ArrayList year = new ArrayList();
+		int tmp;
+		if(b < a){
+			tmp = a;
+			a = b;
+			b = tmp;
+		}
+		for(int i = a;i<=b;i++){
+			if(i%400==0){
+				
+				year.add(i);
+			}
+			else if ((i%4==0)&&(i%100!=0)){
+				year.add(i);
+			}
+		}
+		int size = year.size();
+		System.out.print("共有");
 		
+		for(int j = 0;j<size;j++){
+			System.out.print(year.get(j)+" ");
+		}
+		System.out.println("這"+ size +"個閏年");	
 	}
 }
